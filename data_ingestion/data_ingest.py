6cf9bc4718e40b05fd4c0a6f264fd69296781db0
Soura10 <souradeeproy.10@gmail.com>
2025-12-31 20:25:55 +0530

    remaining folder skeleton done

---
+++ data_ingestion/data_ingest.py
@@ -0,0 +1,5 @@
+from langchain_astradb import AstraDB
+from dotenv import load_dotenv()
+import os
+import pandas as pd
+from data_ingestion.data_transorm import data_converter
---
+++ data_ingestion/data_transform.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+from langchain_core.document import Documents
+
+
+
+class data_converter:
+
+    def __init__(self):
+        pass
+    
+
+    def data_transformation(self):
+        pass
